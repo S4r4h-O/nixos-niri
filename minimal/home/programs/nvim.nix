b6e80377ac650112dbbeda10252587bff0cdab65
Sarah O <sarah.shofani@proton.me>
2026-02-28 22:23:35 -0300

    fix(nvim): fix lockfile incompatibility with nix

--- minimal/home/programs/nvim.nix
+++ minimal/home/programs/nvim.nix
@@ -1,5 +1,9 @@
 { pkgs, inputs, ... }:
 {
   programs.neovim.enable = true;
-  xdg.configFile."nvim".source = "${inputs.dotfiles}/nvim";
+  xdg.configFile."nvim" = {
+    source = "${inputs.dotfiles}/nvim";
+    recursive = true;
+  };
+  xdg.configFile."nvim/lazy-lock.json".enable = false;
 }
